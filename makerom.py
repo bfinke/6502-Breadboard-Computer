aa050bda29458dab683a5de9cfcdc1e1cb0ce3d3
Ben Finke <benjaminfinkeco@gmail.com>
2025-09-26 12:18:03 -0600

    Fixed Git

--- makerom.py
+++ makerom.py
@@ -1,14 +1,18 @@
 # Please see this video for details:
 # https://www.youtube.com/watch?v=yl8vPW5hydQ
 
-rom = bytearray([0xea] * 2 ** 15)
-
-rom[0] = 0xa9
-rom[1] = 0x42
-
-rom[2] = 0x8d
-rom[3] = 0x00
-rom[4] = 0x60
+code = bytearray([0xa9, 0xff,           # lda #$ff (load register a) **setting to output)
+                  0x8d, 0x02, 0x60,     # sta $6002 (store register a at adr 6002)
+                  
+                  0xa9, 0x55,           # lda #$55 (load register a)
+                  0x8d, 0x00, 0x60,     # sta $6000 (store register a at adr 6000)
+                  
+                  0xa9, 0xaa,           # lda #$aa (load register a)
+                  0x8d, 0x00, 0x60,     # sta $6000 (store register a at adr 6000)
+                  
+                  0x4c, 0x05, 0x80      # jump $8005 (the 8005 comes from the fact that each of the hex value listed are stored from 8000, 8001, ... so sending it to 8005 puts it at 0x09 which is a load command 0xa9)
+                  ])    
+rom = code + bytearray([0xea] * (2 ** 15 - len(code)))
 
 rom[0x7ffc] = 0x00
 rom[0x7ffd] = 0x80
